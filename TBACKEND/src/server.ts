bf89e7279867b7bee5980c6cdad0db7ba3e8a12a
Othmane Oubouselham <113171989+OthmaneOubouseham@users.noreply.github.com>
2025-05-17 11:29:10 +0200

    express

--- TBACKEND/src/server.ts
+++ TBACKEND/src/server.ts
@@ -1,1 +1,14 @@
-console.log("Server is starting...");
+import express from 'express';
+import cors from 'cors';
+import { createServer } from 'http';
+import { Server } from 'socket.io';
+
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer, {
+  cors: {
+    origin: '*',
+    methods: ['GET', 'POST']
+  }
+});
