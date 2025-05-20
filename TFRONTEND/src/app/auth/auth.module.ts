42ac6e10b021b8b7cc12bc878997061950dd91af
Othmane Oubouselham <113171989+OthmaneOubouseham@users.noreply.github.com>
2025-05-20 20:51:59 +0200

    register page v1

--- TFRONTEND/src/app/auth/auth.module.ts
+++ TFRONTEND/src/app/auth/auth.module.ts
@@ -1,8 +1,22 @@
 import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { ReactiveFormsModule } from "@angular/forms";
 import { AuthService } from "./services/auth.service";
+import { RegisterComponent } from './components/register/register.component';
+import { RouterModule, Routes } from "@angular/router";
 
+
+const routes: Routes = [
+    {
+        path: 'register',
+        component: RegisterComponent
+        }
+];
 @NgModule({
     providers: [AuthService],
-
-
-})export class AuthModule {}
+    declarations: [
+      RegisterComponent
+    ],
+    imports: [RouterModule.forChild(routes)]
+})
+export class AuthModule {}
